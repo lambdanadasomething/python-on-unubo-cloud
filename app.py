6b025f04df54a30e32eb9efca68dc875a57bd4b9
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-05-18 17:10:57 +0800

    add logging

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ port = int(os.environ.get('PORT', 5000))
 @app.route('/')
 def hello():
     # return 'Welcome to Python on Unubo Cloud'
+    print("Req: /")
     return render_template('index.html')
 
 @app.route('/ver')
 def ver():
+    print("Req: /ver")
     return sys.version
 
 if __name__ == '__main__':
+    print("Starting up at port:" + port)
     app.run(host='0.0.0.0', port=port)
